634a2931f6288419626bed00bd2d36ef54fa52eb
TariqAboumadian <tariqaboumadien@gmail.com>
2023-07-11 23:36:47 +0300

    update data

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICategory } from 'src/app/Models/icategory';
+import { ISubCategory } from 'src/app/Models/isub-category';
+import { CategoryService } from 'src/app/Services/category.service';
+import { ProductService } from 'src/app/Services/product.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   _productName: string="";
   get prodname():string{
@@ -13,5 +17,20 @@ export class HeaderComponent {
   }
   set prodname(value:string){
     this._productName=value;
+  }
+  CategoryList:ICategory[]=[];
+  SubCategoryList:ISubCategory[]=[];
+  constructor(private categoryservice: CategoryService,private productservices:ProductService){}
+
+  ngOnInit(): void {
+    this.categoryservice.GetAllCategories().subscribe(data=>{
+      this.CategoryList=data;
+    });
+    this.categoryservice.GetSubCategoryesByCategoryId(1).subscribe(data=>{
+      this.SubCategoryList=data;
+      for(let ite of this.CategoryList){
+        console.log(ite.name);
+      }
+    });
   }
 }
--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICategory } from 'src/app/Models/icategory';
+import { ISubCategory } from 'src/app/Models/isub-category';
+import { CategoryService } from 'src/app/Services/category.service';
+import { ProductService } from 'src/app/Services/product.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  CategoryList:ICategory[]=[];
+  SubCategoryList:ISubCategory[]=[];
+  constructor(private categoryservice: CategoryService,private productservices:ProductService){}
 
+  ngOnInit(): void {
+    this.categoryservice.GetAllCategories().subscribe(data=>{
+      this.CategoryList=data;
+    });
+    this.categoryservice.GetSubCategoryesByCategoryId(1).subscribe(data=>{
+      this.SubCategoryList=data;
+      for(let ite of this.CategoryList){
+        console.log(ite.name);
+      }
+    });
+  }
 }
