694e12dfb54bde8d7e2022f5102140321bd062e2
TariqAboumadian <tariqaboumadien@gmail.com>
2023-07-20 13:08:52 +0300

    category fillter

--- src/app/Components/products/products.component.ts
+++ src/app/Components/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/Models/iproduct';
+import { ISubCategory } from 'src/app/Models/isub-category';
+import { CategoryService } from 'src/app/Services/category.service';
 import { ProductService } from 'src/app/Services/product.service';
 import { PriceVM } from 'src/app/ViewModel/price-vm';
 
@@ -10,13 +12,14 @@ import { PriceVM } from 'src/app/ViewModel/price-vm';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit{
-  constructor(private activeroute:ActivatedRoute,private productservices:ProductService){}
+  constructor(private activeroute:ActivatedRoute,private productservices:ProductService,private subcategoryservice:CategoryService){}
   catid: number=0;
   products:IProduct[]=[];
   priceminmax:PriceVM|undefined;
   PriceRangeList:number[]=[];
+  subcategories:ISubCategory[]=[];
   page:number=1;
-  itemsperpage:number=4;
+  itemsperpage:number=3;
   ngOnInit(): void {
 
     this.catid=this.activeroute.snapshot.paramMap.get('catid')?Number(this.activeroute.snapshot.paramMap.get('catid')):0;
@@ -37,6 +40,10 @@ export class ProductsComponent implements OnInit{
       }
     console.log(this.PriceRangeList);
     });
+
+    this.subcategoryservice.GetAllSubCategoryes().subscribe(data=>{
+      this.subcategories=data;
+    });
   }
 
   GetPricedProducts(min:number,max:number){
@@ -49,7 +56,10 @@ export class ProductsComponent implements OnInit{
       this.products=data;
     });
   }
-
-
+  GetProductsBysubcategory(id:number){
+    this.productservices.GetProductByCategoryId(id).subscribe(data=>{
+      this.products=data;
+    });
+  }
 }
 
