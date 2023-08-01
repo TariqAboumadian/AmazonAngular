a0b03788e6e50396952b2ab90d10e65ec33b2391
Hosam Gamal <hosamg518@gmail.com>
2023-08-01 12:53:53 +0300

    change in password

--- src/app/Components/change-user-profile/change-user-profile.component.ts
+++ src/app/Components/change-user-profile/change-user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IUserProfile } from 'src/app/Models/iuser-profile';
 import { UserProfileService } from 'src/app/Services/user-profile.service';
@@ -14,31 +15,51 @@ export class ChangeUserProfileComponent implements OnInit {
     id?:string|null;
     userchange:string='';
     key?:string|null;
+    form:FormGroup;
     userProfile:IUserProfile={} as IUserProfile;
     constructor (private activatedRoute: ActivatedRoute,private userProfileService:UserProfileService,
-      private location:Location){
+      private location:Location,private fb:FormBuilder){
+        this.form = this.fb.group({
+          userName:['',[Validators.required]],
+          phone:['',[Validators.required]],
+          emailAddress:['',[Validators.required,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]]
+        })
+        
     }
-
+    get userName(){
+      return this.form.get('userName');
+    }
+    get phone(){
+      return this.form.get('phone');
+    }
+    get emailAddress(){
+      return this.form.get('emailAddress');
+    }
     public ngOnInit() {
       this.activatedRoute.paramMap.subscribe(paramMap=>{
 
-        this.userProfile.userName=paramMap.get('name')||'';
-        this.userProfile.phone=paramMap.get('phone')||'';
-        this.userProfile.emailAddress=paramMap.get('email')||'';
+        this.form.patchValue({
+          userName:paramMap.get('name')||'',
+          phone:paramMap.get('phone')||'',
+          emailAddress:paramMap.get('email')||null
+        })
         this.key= paramMap.get('key');
     })
-    
     this.id=sessionStorage.getItem('userid');
-    }
+    console.log(this.userProfile);
+    console.log(this.form.value);
+  }
 
   saveChanges(){
         if(this.id)
         {
-         if(this.userProfile.emailAddress=="")
-         {
-          this.userProfile.emailAddress=null;
-         }
-         this.userProfileService.updateUser(this.id,this.userProfile).subscribe(data=>{
+          if(this.emailAddress?.value=="null")
+          {
+            this.form.patchValue({
+              emailAddress:null
+            })
+          }
+         this.userProfileService.updateUser(this.id,this.form.value).subscribe(data=>{
           this.location.back();
          })
         }
